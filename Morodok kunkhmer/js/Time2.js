b1c408c477622fe412f9286fe066aaefd2b84bef
netlinklive <99629227+netlinklive@users.noreply.github.com>
2022-07-05 11:55:15 +0700

    Update Time2.js

--- Morodok kunkhmer/js/Time2.js
+++ Morodok kunkhmer/js/Time2.js
@@ -1,9 +1,9 @@
-  var countDownDate2 = new Date("July 9, 2022 14:00:00").getTime();
+  var countDownDate2 = new Date("July 9, 2022 15:00:00").getTime();
   var x2 = setInterval(function() {
   var now2 = new Date().getTime();
   
     var distance2 = countDownDate2 - now2;
-    var endLive2 = new Date("July 5, 2022 16:00:00").getTime() - now2;
+    var endLive2 = new Date("July 5, 2022 17:00:00").getTime() - now2;
     var days2 = Math.floor(distance2 / (1000 * 60 * 60 * 24));
     var hours2 = Math.floor((distance2 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes2 = Math.floor((distance2 % (1000 * 60 * 60)) / (1000 * 60));
