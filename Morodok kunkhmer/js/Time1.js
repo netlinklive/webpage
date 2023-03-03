5e06470fc39b49e17d7b330a7a66864aa37d7cb8
netlinklive <99629227+netlinklive@users.noreply.github.com>
2023-03-04 00:44:51 +0700

    Update Time1.js

--- Morodok kunkhmer/js/Time1.js
+++ Morodok kunkhmer/js/Time1.js
@@ -1,9 +1,9 @@
-  var countDownDate1 = new Date("March 4, 2023 00:00:00").getTime();
+  var countDownDate1 = new Date("March 4, 2023 18:00:00").getTime();
   var x1 = setInterval(function() {
   var now1 = new Date().getTime();
   
     var distance1 = countDownDate1 - now1;
-    var endLive1 = new Date("March 4, 2022 18:00:00").getTime() - now1;
+    var endLive1 = new Date("March 4, 2022 20:30:00").getTime() - now1;
     var days1 = Math.floor(distance1 / (1000 * 60 * 60 * 24));
     var hours1 = Math.floor((distance1 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes1 = Math.floor((distance1 % (1000 * 60 * 60)) / (1000 * 60));
